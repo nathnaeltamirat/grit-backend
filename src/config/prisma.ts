42c73064b88e588a3fdc250a01b52c5cbb71ca04
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-07-29 14:52:52 +0300

    refactor(config): update and export name and JWT_SECRETS

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
-import env from './config.js';
+import envConfig from './config.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client.js';
 
 
 const pool = new Pool({
-  connectionString: env.DATABASE_URL,
+  connectionString: envConfig.DATABASE_URL,
 });
 
 const adapter = new PrismaPg(pool);
