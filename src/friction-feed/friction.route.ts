c4c7472d1e1206ee6e6bb2ee8196c7bf7cd33ea5
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-08-08 17:51:00 +0300

    feat(friction): added friction get  route definition

--- src/friction-feed/friction.route.ts
+++ src/friction-feed/friction.route.ts
@@ -3,8 +3,10 @@ import { authorizeUser } from '../middlewares/auth.middleware.js';
 import {
   createFrictionHandler,
   deleteFrictionHandler,
+  getFrictionHandler,
 } from './friction.controller.js';
 const frictionRouter = Router();
 frictionRouter.post('/', authorizeUser, createFrictionHandler);
 frictionRouter.delete('/:id', authorizeUser, deleteFrictionHandler);
+frictionRouter.get('/', authorizeUser, getFrictionHandler);
 export default frictionRouter;
