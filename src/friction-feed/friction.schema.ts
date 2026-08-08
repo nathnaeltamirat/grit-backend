83b59ff422a738c2550cef9d581fca29a064d60b
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-08-08 16:19:55 +0300

    feat(friction): add createFrictionSchema

---
+++ src/friction-feed/friction.schema.ts
@@ -0,0 +1,11 @@
+import z from 'zod';
+import { SEVERITY } from '../generated/prisma/enums.js';
+
+export const createFrictionSchema = z.object({
+  title: z.string().min(1, 'Title is required'),
+  description: z.string().optional(),
+  tags: z.array(z.string().min(1, "Tag can't be empty")).optional(),
+  severity: z.enum(SEVERITY, {
+    message: ' Severity must be CRITICAL, MEDIUM, or LOW',
+  }),
+});
