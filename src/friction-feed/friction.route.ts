9787cc2797652a93d30eea4ba8dbe9caa86f203e
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-08-08 16:47:20 +0300

    feat(friction): added friction delete route definition

--- src/friction-feed/friction.route.ts
+++ src/friction-feed/friction.route.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { authorizeUser } from '../middlewares/auth.middleware.js';
-import { createFrictionHandler } from './friction.controller.js';
+import {
+  createFrictionHandler,
+  deleteFrictionHandler,
+} from './friction.controller.js';
 const frictionRouter = Router();
 frictionRouter.post('/', authorizeUser, createFrictionHandler);
-
+frictionRouter.delete('/:id', authorizeUser, deleteFrictionHandler);
 export default frictionRouter;
