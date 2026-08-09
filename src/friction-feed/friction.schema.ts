ac4b0737f0f183ecd2756d3ac19c2f003337610a
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-08-09 14:23:49 +0300

    feat(friction): add paritail schema and refactor current schema change

--- src/friction-feed/friction.schema.ts
+++ src/friction-feed/friction.schema.ts
@@ -1,11 +1,14 @@
 import z from 'zod';
 import { SEVERITY } from '../generated/prisma/enums.js';
 
-export const createFrictionSchema = z.object({
+export const baseFrictionSchema = z.object({
   title: z.string().min(1, 'Title is required'),
   description: z.string().optional(),
   tags: z.array(z.string().min(1, "Tag can't be empty")).optional(),
   severity: z.enum(SEVERITY, {
-    message: ' Severity must be CRITICAL, MEDIUM, or LOW',
+    message: 'Severity must be CRITICAL, MEDIUM, or LOW',
   }),
 });
+
+export const createFrictionSchema = baseFrictionSchema;
+export const updateFrictionSchema = baseFrictionSchema.partial();
