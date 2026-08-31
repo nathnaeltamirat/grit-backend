1e8ce8e76923d4460093e2785e612d7d49a8cc3c
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-08-31 16:42:56 +0300

    feat(insights): schema for insight generator for specific frictions

---
+++ src/insights/insights.schema.ts
@@ -0,0 +1,6 @@
+import z from 'zod';
+
+export const specificFrictionInsightSchema = z.object({
+  friction_id: z.array(z.string().trim()).min(1, 'Friction not chosen'),
+  customInstraction: z.string().optional(),
+});
