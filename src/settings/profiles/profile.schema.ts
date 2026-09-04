edf14526321b42fbf833eee45eeadce331a4a23a
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-04 17:24:23 +0300

    feat(settings): update profile schema added

---
+++ src/settings/profiles/profile.schema.ts
@@ -0,0 +1,18 @@
+import z, { email } from 'zod';
+
+export const updateProfileSchema = z.object({
+  full_name: z
+    .string()
+    .trim()
+    .min(3, 'Name is too short')
+    .refine((val) => val.split(' ').filter(Boolean).length == 2, {
+      message: 'Please enter your first and last name',
+    })
+    .optional(),
+  email: z
+    .string()
+    .trim()
+    .toLowerCase()
+    .pipe(z.email('Invalid email'))
+    .optional(),
+});
