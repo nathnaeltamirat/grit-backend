dfc5c90acdf226a1d9344dae3505e51bb10aeff5
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-04 22:21:48 +0300

    feat(settings): updatePasssword route mapped

--- src/setting/profile/profile.route.ts
+++ src/setting/profile/profile.route.ts
@@ -1,6 +1,7 @@
-import { Router } from "express";
-import { authorizeUser } from "../../middlewares/auth.middleware.js";
-import { updateProfileHandler } from "./profile.controller.js";
+import { Router } from 'express';
+import { authorizeUser } from '../../middlewares/auth.middleware.js';
+import { updatePasswordhandler, updateProfileHandler } from './profile.controller.js';
 const profileRouter = Router();
-profileRouter.patch('/',authorizeUser,updateProfileHandler);
-export default profileRouter
+profileRouter.patch('/', authorizeUser, updateProfileHandler);
+profileRouter.patch('/password', authorizeUser, updatePasswordhandler);
+export default profileRouter;
