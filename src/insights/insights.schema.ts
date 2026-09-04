19f4a70580f935a637bde27ee12f9f2ba8174a41
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-04 16:45:48 +0300

    feat(insights):partially implement insights schema

--- src/insights/insights.schema.ts
+++ src/insights/insights.schema.ts
@@ -3,4 +3,5 @@ import z from 'zod';
 export const specificFrictionInsightSchema = z.object({
   friction_id: z.array(z.string().trim()).min(1, 'Friction not chosen'),
   customInstraction: z.string().optional(),
+  modelName:z.string().optional()
 });
