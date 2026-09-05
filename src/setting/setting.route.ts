17cba5cc32f1ae1dd1fe9d3a4bd5c090a95666bb
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-05 08:47:53 +0300

    feat(setting): api-key route configured

--- src/setting/setting.route.ts
+++ src/setting/setting.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import profileRouter from "./profile/profile.route.js";
+import { updateAIAPIKeyHandler } from "./setting.controller.js";
+import { authorizeUser } from "../middlewares/auth.middleware.js";
 const settingRouter = Router();
 
 settingRouter.use('/profile',profileRouter)
-
+settingRouter.patch('/api-key',authorizeUser,updateAIAPIKeyHandler)
 export default settingRouter
