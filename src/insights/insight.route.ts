cfa1525d24168f833cd9f7ab966b3428d085ec22
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-15 16:15:29 +0300

    feat(insight): map timeRangeFrictionInsightHandler to /insights/timeRange route

--- src/insights/insight.route.ts
+++ src/insights/insight.route.ts
@@ -1,8 +1,16 @@
-import { Router } from "express";
-import { authorizeUser } from "../middlewares/auth.middleware.js";
-import { specificFrictionInsightHandler } from "./insight.controller.js";
+import { Router } from 'express';
+import { authorizeUser } from '../middlewares/auth.middleware.js';
+import {
+  specificFrictionInsightHandler,
+  timeRangeFrictionInsightHandler,
+} from './insight.controller.js';
 const insightRouter = Router();
 
-insightRouter.post("/specific",authorizeUser,specificFrictionInsightHandler)
+insightRouter.post('/specific', authorizeUser, specificFrictionInsightHandler);
+insightRouter.post(
+  '/timeRange',
+  authorizeUser,
+  timeRangeFrictionInsightHandler,
+);
 
 export default insightRouter;
