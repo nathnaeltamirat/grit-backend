1deb7f7e02582627f17ee98944092fdae773a16c
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-15 16:03:38 +0300

    feat(insight): add timeRangeFrictionInsight schema

--- src/insights/insight.schema.ts
+++ src/insights/insight.schema.ts
@@ -3,7 +3,21 @@ import z from 'zod';
 export const specificFrictionInsightSchema = z.object({
   friction_id: z.array(z.string().trim()).min(1, 'Friction not chosen'),
   customInstraction: z.string().optional(),
-  modelName:z.enum(["openai/gpt-oss-20b","openai/gpt-oss-120b","qwen/qwen3.8-27b"],{
-    error:"model name not recognized"
-  })
+  modelName: z.enum(
+    ['openai/gpt-oss-20b', 'openai/gpt-oss-120b', 'qwen/qwen3.8-27b'],
+    {
+      error: 'model name not recognized',
+    },
+  ),
+});
+export const timeRangeFrictionInsightSchema = z.object({
+  start: z.coerce.date({ error: 'Start date is required' }),
+  end: z.coerce.date({ error: 'End date is required' }),
+  customInstraction: z.string().optional(),
+  modelName: z.enum(
+    ['openai/gpt-oss-20b', 'openai/gpt-oss-120b', 'qwen/qwen3.8-27b'],
+    {
+      error: 'model name not recognized',
+    },
+  ),
 });
