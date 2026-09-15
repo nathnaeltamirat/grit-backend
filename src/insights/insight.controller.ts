b88c505381df24645337b529348d52b10c5c163b
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-15 16:14:50 +0300

    feat(insight): add timeRangeFrictionInsightHandler

--- src/insights/insight.controller.ts
+++ src/insights/insight.controller.ts
@@ -2,7 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import AppError from '../types/error.js';
-import { specificFrictionInsightSchema } from './insight.schema.js';
+import {
+  specificFrictionInsightSchema,
+  timeRangeFrictionInsightSchema,
+} from './insight.schema.js';
 import prisma from '../config/prisma.js';
 import { AIInput } from '../services/ai.api.js';
 import AIResponse from '../services/ai.api.js';
@@ -46,6 +49,68 @@ export const specificFrictionInsightHandler = async (
           id,
         },
       });
+      body.data.push({
+        title: friction?.title ?? '',
+        description: friction?.description ?? '',
+      });
+    }
+
+    const insight = await AIResponse(body, api_key.ai_api_key);
+    return res.status(201).json({
+      success: true,
+      message: 'insight created successfully',
+      data: insight,
+    });
+  } catch (err) {
+    if (err instanceof ZodError) {
+      const error: AppError = new Error(
+        err.issues.map((issue) => issue.message).join(', '),
+      );
+      error.status = 400;
+      return next(error);
+    }
+    return next(err);
+  }
+};
+export const timeRangeFrictionInsightHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const data = timeRangeFrictionInsightSchema.parse(req.body);
+    const { start, end, customInstraction, modelName } = data;
+    const customPrompt = customInstraction
+      ? `${customInstraction} \n Follow the system interaction mainly`
+      : 'Follow the system interaction mainly';
+    const body: AIInput = {
+      data: [],
+      modelName,
+      customPrompt: customPrompt,
+    };
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.id,
+      },
+    });
+    if (!user?.ai_api_key) {
+      throw errorUitl('Unauthorized make sure your api key is configured', 401);
+    }
+    const frictions = await prisma.friction_Log.findMany({
+      where: {
+        user_id: req.id,
+        created_at: {
+          gte: start,
+          lt: end,
+        },
+      },
+    });
+    const api_key = jwt.verify(
+      user.ai_api_key,
+      envConfig.JWT_SECRET,
+    ) as TokenPayload;
+
+    for (const friction of frictions) {
       body.data.push({
         title: friction?.title ?? '',
         description: friction?.description ?? '',
