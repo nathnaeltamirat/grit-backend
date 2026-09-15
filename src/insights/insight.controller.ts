e9a96673686b316f728b453cc5cc901164eec616
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2026-09-15 15:57:32 +0300

    fix(insight): fix insight controller api extraction

--- src/insights/insight.controller.ts
+++ src/insights/insight.controller.ts
@@ -1,12 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import AppError from '../types/error.js';
 import { specificFrictionInsightSchema } from './insight.schema.js';
 import prisma from '../config/prisma.js';
 import { AIInput } from '../services/ai.api.js';
 import AIResponse from '../services/ai.api.js';
 import { errorUitl } from '../utils/error.util.js';
-
+import envConfig from '../config/config.js';
+interface TokenPayload extends JwtPayload {
+  ai_api_key: string;
+}
 export const specificFrictionInsightHandler = async (
   req: Request,
   res: Response,
@@ -31,6 +35,11 @@ export const specificFrictionInsightHandler = async (
     if (!user?.ai_api_key) {
       throw errorUitl('Unauthorized make sure your api key is configured', 401);
     }
+    const api_key = jwt.verify(
+      user.ai_api_key,
+      envConfig.JWT_SECRET,
+    ) as TokenPayload;
+
     for (const id of friction_id) {
       const friction = await prisma.friction_Log.findUnique({
         where: {
@@ -43,7 +52,7 @@ export const specificFrictionInsightHandler = async (
       });
     }
 
-    const insight = await AIResponse(body, user?.ai_api_key);
+    const insight = await AIResponse(body, api_key.ai_api_key);
     return res.status(201).json({
       success: true,
       message: 'insight created successfully',
